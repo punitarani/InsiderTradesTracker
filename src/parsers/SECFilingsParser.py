caadc80a579b896dbadbce6588022961ee8c84ed
Punit Sai Arani <punitsai36@gmail.com>
2022-06-02 13:12:18 -0700

    Add parse functionality

--- src/parsers/SECFilingsParser.py
+++ src/parsers/SECFilingsParser.py
@@ -1,12 +1,18 @@
 # SEC Filings Parser
 
-import requests
+from datetime import datetime
+from xml.etree import ElementTree
 
+import pandas as pd
+import requests
+from lxml import etree
+
+from baseurls import SEC_LATEST_FILINGS_URL
 from src.parsers.WebpageParser import WebpageParser, ResponseError
 
 
 class SECFilingsParser(WebpageParser):
-    def __init__(self, name: str, url: str):
+    def __init__(self, name: str, url: str = None):
         """
         SEC Filings Parser Class Constructor
 
@@ -15,6 +21,15 @@ class SECFilingsParser(WebpageParser):
         """
 
         super().__init__(name, url)
+
+        # Use the latest SEC Filings URL from baseurls.py to get the latest SEC Filings
+        if url is None:
+            self.url = SEC_LATEST_FILINGS_URL
+
+            # TODO: Add logging
+            print(f'{self.name} URL is not set. Using default URL: {self.url}')
+
+        self.filings: pd.DataFrame = pd.DataFrame()
 
     def get_webpage(self, *args, **kwargs) -> str:
         """
@@ -39,9 +54,63 @@ class SECFilingsParser(WebpageParser):
 
         response = requests.get(self.url, headers=headers)
 
+        # Get the content type of the response
+        self.content_type = response.headers['Content-Type']
+
         # Check if response is successful
         if response.status_code != 200:
             raise ResponseError(f'Response Error: {response.status_code} - {response.reason}')
 
         self.webpage = response.text
+
         return response.text
+
+    def parse(self) -> pd.DataFrame:
+        """
+        Parse the SEC Filings into DataFrame
+
+        :return: Filings DataFrame
+        """
+
+        # Initialize DataFrame
+        filing_cols = ['acc', 'type', 'title', 'date_time', 'link']
+        filings = pd.DataFrame(columns=filing_cols)
+
+        data: ElementTree = etree.fromstring(self.get_webpage().encode('utf-8'))
+
+        # Iterate through entries
+        entries = data.findall('{http://www.w3.org/2005/Atom}entry')
+
+        for entry in entries:
+            namespaces = {'atom': 'http://www.w3.org/2005/Atom'}
+
+            # Get filing title
+            title = entry.xpath('atom:title/text()', namespaces=namespaces)[0]
+
+            # Get filing documents link
+            link = entry.xpath('atom:link/@href', namespaces=namespaces)[0]
+
+            # Summary
+            # entry.xpath('atom:summary/text()', namespaces=namespaces)[0]
+
+            # Get Filing Date Time
+            date_time = datetime.strptime(entry.xpath('atom:updated/text()', namespaces=namespaces)[0][:-6],
+                                          '%Y-%m-%dT%H:%M:%S')
+
+            # Get form type
+            form_type = entry.xpath('atom:category/@term', namespaces=namespaces)[0]
+
+            # Get Accession Number
+            acc_no = entry.xpath('atom:id/text()', namespaces=namespaces)[0].split('=')[-1]
+
+            # Update DataFrame
+            filings = pd.concat([filings, pd.DataFrame({'acc': [acc_no], 'type': [form_type], 'title': [title],
+                                                        'date_time': [date_time], 'link': [link]})])
+
+        # Set acc as index
+        filings.set_index('acc', inplace=True)
+
+        # Cache the filings DataFrame
+        self.filings = filings
+
+        return filings
