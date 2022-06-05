03e763781464b5585130a31cb7ec2550ab81c61d
Punit Sai Arani <punitsai36@gmail.com>
2022-06-05 16:41:25 -0700

    Add SECFilingParser tests

--- tests/parser/test_parsers.py
+++ tests/parser/test_parsers.py
@@ -1,11 +1,11 @@
 import unittest
 
+import numpy as np
 import pandas as pd
 
-from tracker.parsers.sec_latest_filings_parser import SECFilingsParser
-from tracker.parsers.webpage_parser import WebpageParser
-
 from baseurls import SEC_LATEST_FILINGS
+from tracker.parsers import SECFilingsParser, SECFilingParser
+from tracker.parsers.webpage_parser import WebpageParser
 
 
 class BasicParserTests(unittest.TestCase):
@@ -69,7 +69,7 @@ class BasicParserTests(unittest.TestCase):
         self.assertEqual(len(html_urls), 10)
 
 
-class SECFilingsParserTests(unittest.TestCase):
+class SECLatestFilingsParserTests(unittest.TestCase):
     def test_parse(self):
         filings = SECFilingsParser('Filings')
         self.assertTrue(isinstance(filings.filings, pd.DataFrame))
@@ -78,6 +78,57 @@ class SECFilingsParserTests(unittest.TestCase):
         filings.parse()
         self.assertGreater(filings.filings.shape[0], 0)
         self.assertGreater(filings.filings.shape[1], 0)
+
+
+class SECFilingParserTests(unittest.TestCase):
+    def test_init(self):
+        parser_name = 'FilingParser'
+        parser_url = 'https://www.sec.gov/Archives/edgar/data/' \
+                     '0000320193/000032019321000071/0000320193-21-000071-index.html'
+        parser = SECFilingParser(parser_name, parser_url)
+        self.assertIsNotNone(parser)
+        self.assertEqual(parser.name, parser_name)
+        self.assertEqual(parser.url, parser_url)
+
+        # Test get_webpage()
+        webpage = parser.get_webpage()
+        self.assertIsNotNone(webpage)
+
+        # Test get_soup()
+        soup = parser.get_soup()
+        self.assertIsNotNone(soup)
+
+    def test_parse(self):
+        parser_name = 'FilingParser'
+        parser_url = 'https://www.sec.gov/Archives/edgar/data/' \
+                     '0000320193/000032019321000071/0000320193-21-000071-index.html'
+        parser = SECFilingParser(parser_name, parser_url)
+
+        # Test parse()
+        parser.parse()
+        self.assertIsNotNone(parser.data)
+        self.assertEqual(parser.data.shape, (3, 6))
+        self.assertEqual(parser.data.columns.tolist(), ['Seq', 'Description', 'Document', 'Type', 'Size', 'Link'])
+
+        # Verify Accuracy
+        # Test row 1
+        self.assertEqual(parser.data.iloc[0, 0], 1)
+        self.assertEqual(parser.data.iloc[0, 1], 'FORM 4')
+        self.assertEqual(parser.data.iloc[0, 2], 'wf-form4_162984422696515.html')
+        self.assertEqual(parser.data.iloc[0, 3], 4)
+
+        # Test row 2
+        self.assertEqual(parser.data.iloc[1, 0], 1)
+        self.assertEqual(parser.data.iloc[1, 1], 'FORM 4')
+        self.assertEqual(parser.data.iloc[1, 2], 'wf-form4_162984422696515.xml')
+        self.assertEqual(parser.data.iloc[1, 3], 4)
+        self.assertEqual(parser.data.iloc[1, 4], 3975)
+
+        # Test row 3
+        self.assertTrue(np.isnan(parser.data.iloc[2, 0]))
+        self.assertEqual(parser.data.iloc[2, 1], 'Complete submission text file')
+        self.assertEqual(parser.data.iloc[2, 2], '0000320193-21-000071.txt')
+        self.assertEqual(parser.data.iloc[2, 4], 5473)
 
 
 if __name__ == '__main__':
