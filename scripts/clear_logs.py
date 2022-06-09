6baf9e4d71040ad4bcce90a3776a65348e3979fa
Punit Sai Arani <punitsai36@gmail.com>
2022-06-09 12:22:48 -0700

    Initial commit

---
+++ scripts/clear_logs.py
@@ -0,0 +1,40 @@
+# Clear Logs Script
+
+from pathlib import Path
+
+from defs import LOG_DIR_PATH
+
+
+def clear_logs() -> bool:
+    """
+    Clears the logs directory
+    """
+
+    delete_success: bool = True
+
+    # Get log files
+    try:
+        log_files = [file for file in Path(LOG_DIR_PATH).glob('*.log')]
+        print(f'Clearing {len(log_files)} log files...')
+    except FileNotFoundError:
+        return False
+
+    # Iterate over log files
+    for log_file in log_files:
+        # Clear log file
+        try:
+            log_file.unlink()
+            log_file.touch()
+            print(f'Cleared log file: {log_file}.')
+
+        # Error clearing log file
+        except Exception as e:
+            print(f'Error deleting log file: {log_file}.')
+            print(e)
+            delete_success = False
+
+    return delete_success
+
+
+if __name__ == '__main__':
+    clear_logs()
