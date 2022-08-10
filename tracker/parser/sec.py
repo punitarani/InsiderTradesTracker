3d31126b72a9c70271dc6c798631d6fa38d3940e
Punit Arani <punitsai36@gmail.com>
2022-08-10 07:12:51 -0700

    Remove checking email in _get_user_agent(). Fix half-commit #169c507

--- tracker/parser/sec.py
+++ tracker/parser/sec.py
@@ -63,7 +63,7 @@ class SECParser(WebpageParser):
         """
 
         # Check if required config values are available.
-        if config.NAME is not None and config.EMAIL is not None:
+        if config.NAME is not None:
             return f'{config.NAME}'
 
         # Else: Use Chrome user agent (NOT RECOMMENDED).
